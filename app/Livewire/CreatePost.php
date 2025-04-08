a90513d37228d1d8602e435a4ecb68922f8767ce
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-08 16:54:38 -0400

    Componentes de clase vs componentes inline, para uno se usa una clase y una vista y así se divide la lógica de la vista, en el otro caso todo se procesa y renderiza dentro de la misma clase, los casos de uso dependen de la complejidad del componente

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -8,12 +8,10 @@ class CreatePost extends Component
 {
     public function render()
     {
-        // return view('livewire.create-post');
-        // Componentes inline, renderizan todo el contenido sin una vista por ser muy simples
-        return <<< 'HTML'
-            <div>
-                <h1>Hola desde el componente</h1>
-            </div>
+        return <<<'HTML'
+        <div>
+            Prueba
+        </div>
         HTML;
     }
 }
--- resources/views/livewire/post/create-post.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    {{-- Todo contenido de un componente livewire debe estar dentro de un contenedor padre --}}
-</div>
