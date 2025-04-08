391341dc84a85f9b6b6cb968388fb71407188a78
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-08 16:43:01 -0400

    Creación de componentes, tienen una clase que manejará la lógica y la reactividad y una vista que será el contenido rnderizado, el comando para crear componentes es: php artisan livewire:make nombre-componente, la vista debe siempre tener un contenedor padre

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+            {{-- Llamar al componente desde la vista --}}
+            @livewire('create-post')
+
+            {{-- Cada componente tiene efecto sobre su conenido y nada más --}}
+            <p>Contenido fuera del componente</p>
         </div>
     </div>
 </x-app-layout>
---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- Todo contenido de un componente livewire debe estar dentro de un contenedor padre --}}
+    <h1>Primer componente</h1>
+</div>
