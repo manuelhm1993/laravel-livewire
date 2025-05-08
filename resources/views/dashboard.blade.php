a89450df8dd1ababf625c500f428a679f5926d4b
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-07 23:03:30 -0400

    Reinicio del curso con php v8.3.17 y laravel 12.13.0, no se usó kit de inicio y por lo tanto se instaló jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,15 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            {{-- Llamar al componente desde la vista --}}
-            @livewire('create-post')
-
-            {{-- Cada componente tiene efecto sobre su conenido y nada más --}}
-            <p>Contenido fuera del componente</p>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div>
         </div>
     </div>
 </x-app-layout>
